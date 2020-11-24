38db1c7752f4a3160dcfeef3828fd44b71b4a160
vathana <vathana@vathana-pc.local>
2020-11-24 17:32:02 -0500

    v3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
 import logo from './dump.png'
+import alanBtn from '@alan-ai/alan-sdk-web'
+import {useEffect} from 'react';
+
+const alanKey = 'b2c7b55faee4bc4fba84d8ac236dc59f2e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command}) => {
+        if(command === 'testCommand') {
+          alert('This code works')
+        }
+      }
+    })
+  })
   return (
     <div className="App">
       <header className="App-header">
